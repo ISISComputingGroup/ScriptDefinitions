046f6fd9d6c53a0dc4de9985345a4e012005f5e4
JamesKingWork <JamesKingWork@ndlt1208.isis.cclrc.ac.uk>
2020-01-27 13:31:36 +0000

    Adding help

--- example1.py
+++ example1.py
@@ -16,3 +16,5 @@ class DoRun(ActionDefinition):
         else:
             return None
 
+    def get_help(self):
+        return None
--- example2.py
+++ example2.py
@@ -20,8 +20,6 @@ class DoRun(ActionDefinition):
 
     @cast_parameters_to(temperature=float, field=float, uamps=mytype)
     def parameters_valid(self, temperature: float=0.0, field: float=0.0, uamps: float=0.0):
-        with open("C:\\Instrument\\Dev\\PythonLog.txt", "a+") as f:
-            f.write("Checking validity example2 \r\n")
         errors: str = ""
         if not 0.1 <= temperature <= 300:
             errors += "Temperature outside range\n"
@@ -29,9 +27,11 @@ class DoRun(ActionDefinition):
             errors += "Field outside range"
         if not -20 <= uamps <= 32:
             errors += "uamps outside of range"
-        with open("C:\\Instrument\\Dev\\PythonLog.txt", "a+") as f:
-            f.write(errors+"\r\n")
         if errors != "":
             return errors
         return None    
 
+    def get_help(self):
+        return """
+A useful help string
+"""
