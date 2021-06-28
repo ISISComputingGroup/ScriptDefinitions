3e4a4ef74f9deb0b78293ec52cbe3346c644032a
Dominic Oram <dominic.oram@stfc.ac.uk>
2021-06-28 13:36:10 +0100

    Added test for global params

---
+++ test_global_params.py
@@ -0,0 +1,19 @@
+from genie_python.genie_script_generator import ScriptDefinition
+from collections import OrderedDict
+
+
+class DoRun(ScriptDefinition):
+    global_params_definition = OrderedDict({"example param:": ("0", int), "example param 2:": ("2", float),
+                                            "example param 3:": ("any string", str)})
+
+    def run(self, to_print="hello"):
+        print(to_print)
+
+    def parameters_valid(self, to_print="hello"):
+        return self.global_params["example param:"] == 1
+            
+    def estimate_time(self, to_print="hello"):
+        return self.global_params["example param 2:"]
+
+    def get_help(self):
+        return "This is my help for test_dynamic_scripting"
