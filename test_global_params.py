9f24439ba065577adc6ee5637793871999389369
JamesKingWork <james.c.king@stfc.ac.uk>
2021-08-06 15:25:54 +0100

    Validate param2 individually

--- test_global_params.py
+++ test_global_params.py
@@ -1,9 +1,15 @@
-from genie_python.genie_script_generator import ScriptDefinition
+from genie_python.genie_script_generator import ScriptDefinition, GlobalParamValidationError
 from collections import OrderedDict
+
+def param2_validator(param2) :
+    param2 = float(param2)
+    if param2 < 1 or param2 > 3:
+        raise GlobalParamValidationError("Param 2 must be between 1 and 3")
+    return param2
 
 
 class DoRun(ScriptDefinition):
-    global_params_definition = OrderedDict({"example param:": ("1", int), "example param 2:": ("1.23", float),
+    global_params_definition = OrderedDict({"example param:": ("1", int), "example param 2:": ("1.23", param2_validator),
                                             "example param 3:": ("hello", str), "example param 4:": ("0", int)})
 
     def run(self, to_print="hello"):
