5112dbb62eb70d05253ee9fb55bfb980bfbac593
Isaac Hill <isaac.hill@stfc.ac.uk>
2023-10-17 11:02:38 +0100

    newExampleScript

--- enum_example.py
+++ enum_example.py
@@ -1,32 +1,34 @@
 from genie_python.genie_script_generator import ScriptDefinition, cast_parameters_to
 
-class DoRun(ScriptDefinition):
+from enum import Enum
 
-    # Create a property to store valid enum names
-    process_status_values = ["ok", "not_okay", "definitely_not_okay"]
+
+class DoRun(ScriptDefinition):
+    
 
     enum_params = ["process_status"]
+    class process_status_enum(Enum):
+        ok = 1
+        not_okay = 2
+        definitely_not_okay = 3
 
-    def run(self, process_status="start"):
+
+    def run(self, process_status="not_working"):
         print("Running do_run with process_status: " + process_status)
 
     @cast_parameters_to(process_status=str)
     def parameters_valid(self, process_status="ok"):
-        # if process_status_enum not in self.process_status_enum_values:
-        #     return "process_status must be one of: " + str(self.process_status_enum_values)
-        # return None
-        if process_status == "ok":
-            return None
-        else: 
-            return "process_status must be one of: " + str(self.process_status_values)
+        # check that the enum value of process_status is ok
+        if self.process_status_enum[process_status] != self.process_status_enum.ok:
+            return "process_status must be ok"
 
     def estimate_time(self, process_status="ok"):
         if process_status == "ok":
             return 10
-        elif process_status == "not_okay":
+        else:
             return 0
 
     def get_help(self):
         return """
-This is the example ENUM script.\n
+This is the example ENUM dropdown script.\n
         """
